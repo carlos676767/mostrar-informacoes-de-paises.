362082b8dc12e5c185c8283967e0abc9016b34fd
carlos676767 <carlitobr730@gmail.com>
2024-03-17 10:03:28 -0700

    darkmode agora salva os valores das cores no local storage

--- continentes/src/main.ts
+++ continentes/src/main.ts
@@ -4,11 +4,26 @@ function checkBoxDarkMode() {
   darkModeButton.addEventListener("change", () => {
     if (darkModeButton.checked) {
       document.body.classList.add("dark-mode")
+      localStorage.setItem("minhaClasse","dark-mode")
       document.body.classList.remove("white-mode")
     } else {
       document.body.classList.add("white-mode")
+      localStorage.setItem("minhaClasse", "white-mode")
       document.body.classList.remove("dark-mode")
     }
   });
 }
 checkBoxDarkMode();
+
+const valoresSalvosDarkMode = () => {
+  const recupararValorMode = localStorage.getItem("minhaClasse")
+  if (recupararValorMode === "dark-mode") {
+    document.body.classList.add("dark-mode")
+    document.body.classList.remove("white-mode")
+  }else{
+    document.body.classList.add("white-mode")
+    document.body.classList.remove("dark-mode")
+  }
+}
+
+valoresSalvosDarkMode()
