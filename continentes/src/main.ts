d70228cce90425db656e06358d716d6aea7eede7
carlos676767 <carlitobr730@gmail.com>
2024-03-17 09:46:32 -0700

    interacao no checkbox colocfado

--- continentes/src/main.ts
+++ continentes/src/main.ts
@@ -1,1 +1,16 @@
 
+
+
+
+
+function checkBoxDarkMode() {
+  const darkModeButton = document.getElementById("dark-mode-toggle") as HTMLInputElement;
+  darkModeButton.addEventListener("change", () => {
+    if (darkModeButton.checked) {
+ 
+    } else {
+      
+    }
+  });
+}
+checkBoxDarkMode();
