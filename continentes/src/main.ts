2c9a61d0a371b3185003d7fc069cd131edf557e5
carlos676767 <carlitobr730@gmail.com>
2024-03-17 09:52:11 -0700

    darkmode feito.

--- continentes/src/main.ts
+++ continentes/src/main.ts
@@ -1,15 +1,13 @@
 
-
-
-
-
 function checkBoxDarkMode() {
   const darkModeButton = document.getElementById("dark-mode-toggle") as HTMLInputElement;
   darkModeButton.addEventListener("change", () => {
     if (darkModeButton.checked) {
- 
+      document.body.classList.add("dark-mode")
+      document.body.classList.remove("white-mode")
     } else {
-      
+      document.body.classList.add("white-mode")
+      document.body.classList.remove("dark-mode")
     }
   });
 }
