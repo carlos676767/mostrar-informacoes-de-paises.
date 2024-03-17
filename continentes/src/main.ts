dbc592252a60f51283bd5ae6f699101b92058711
carlos676767 <carlitobr730@gmail.com>
2024-03-17 10:07:16 -0700

    setWhiteMode funcao colocada para manter o codigo mais legivel

--- continentes/src/main.ts
+++ continentes/src/main.ts
@@ -5,9 +5,7 @@ function checkBoxDarkMode() {
     if (darkModeButton.checked) {
       setDarkMode();
     } else {
-      document.body.classList.add("white-mode")
-      localStorage.setItem("minhaClasse", "white-mode")
-      document.body.classList.remove("dark-mode")
+      setWhiteMode()
     }
   });
 }
@@ -18,6 +16,12 @@ const setDarkMode = () => {
   document.body.classList.add("dark-mode")
   localStorage.setItem("minhaClasse","dark-mode")
   document.body.classList.remove("white-mode")
+}
+
+const setWhiteMode = () => {
+  document.body.classList.add("white-mode")
+  localStorage.setItem("minhaClasse", "white-mode")
+  document.body.classList.remove("dark-mode")
 }
 
 const valoresSalvosDarkMode = () => {
